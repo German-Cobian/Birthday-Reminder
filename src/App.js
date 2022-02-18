96481a25c8fcb49bb9c62b52d787e9d419355e7d
German Renato Cobian <germancobian@hotmail.com>
2022-02-18 14:11:42 -0800

    Add image of completed project to ReadMe

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <section className="container">
         <h3>{people.length} Birthdays today</h3>
         <List people={people}/>
-        <button onClick={() => console.log('You clicked me!')}>Clear All</button>
+        <button onClick={() => setPeople([])}>Clear All</button>
       </section>
     </main>
   );
